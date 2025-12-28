a808d0c76b0c25dd6081a4fc142c265f0786cdaf
Your Name <ssaiet.ritesh@gmail.com>
2025-12-29 01:36:31 +0530

    chat bot streamText

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,23 +1,52 @@
+// import { google } from "@ai-sdk/google";
+// import { generateText } from 'ai';
+
+// export async function POST(req: Request) {
+//   try {
+//     const { prompt } = await req.json();
+
+//     console.log("Prompt received at server (GEMINI):", prompt);
+
+//     const { text } = await generateText({
+//       model: google('gemini-2.5-flash'),
+//       system:
+//     'You are a professional content writer. ' +
+//     'You need to Highligh heavy tasks , bold important points and make sure that the content is easily digestible and easy to understand.',
+//       prompt,
+//     });
+
+//     return Response.json({ text });
+//   } catch (error) {
+//     console.error('Error:', error);
+//     return Response.json({ error: 'Failed to generate response' }, { status: 500 });
+//   }
+// }
+
 import { google } from "@ai-sdk/google";
-import { generateText } from 'ai';
+import { streamText, convertToModelMessages } from "ai";
+
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
   try {
-    const { prompt } = await req.json();
+    const { messages } = await req.json();
 
-    console.log("Prompt received at server (GEMINI):", prompt);
+    console.log("Messages received at server (GEMINI):", messages);
 
-    const { text } = await generateText({
-      model: google('gemini-2.5-flash'),
+    const result = streamText({
+      model: google("gemini-2.5-flash"),
       system:
-    'You are a professional content writer. ' +
-    'You need to Highligh heavy tasks , bold important points and make sure that the content is easily digestible and easy to understand.',
-      prompt,
+        "You are a professional content writer. " +
+        "You need to highlight heavy tasks, bold important points and make sure that the content is easily digestible and easy to understand.",
+      messages: await convertToModelMessages(messages),
     });
 
-    return Response.json({ text });
+   return result.toUIMessageStreamResponse();
   } catch (error) {
-    console.error('Error:', error);
-    return Response.json({ error: 'Failed to generate response' }, { status: 500 });
+    console.error("Error:", error);
+    return Response.json(
+      { error: "Failed to generate response" },
+      { status: 500 }
+    );
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 'UseClient'
 
 import { Chat } from '@/components/Chat';
+import { Chat2 } from '@/components/Chat2';
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <Chat/>
+      {/* <Chat/> */}
+      <Chat2/>
     </div>
   );
 };
 
 export default Home;
+
